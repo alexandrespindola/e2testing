1241bf103494ab15a678a2d2dcbf465d2ccc3295
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-08-29 05:49:10 +0200

    chore: Update routing configuration to include login and detail views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,34 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DetailView from '@/views/DetailView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/detail/:id',
+    name: 'detail',
+    component: DetailView,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+    meta: {
+      requiresAuth: false
+    }
   }
+
 ]
 
 const router = createRouter({
